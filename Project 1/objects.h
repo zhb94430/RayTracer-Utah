b38e503ef2e4bb22761f6c4cf731c0c9a79fcb74
Hongbin Zhang <zhan2385@umn.edu>
2017-07-31 19:19:52 -0600

    initial commit

---
+++ Project 1/objects.h
@@ -0,0 +1,30 @@
+//-------------------------------------------------------------------------------
+///
+/// \file       objects.h 
+/// \author     Cem Yuksel (www.cemyuksel.com)
+/// \version    1.0
+/// \date       August 26, 2013
+///
+/// \brief Example source for CS 6620 - University of Utah.
+///
+//-------------------------------------------------------------------------------
+
+#ifndef _OBJECTS_H_INCLUDED_
+#define _OBJECTS_H_INCLUDED_
+
+#include "scene.h"
+
+//-------------------------------------------------------------------------------
+
+class Sphere : public Object
+{
+public:
+	virtual bool IntersectRay( const Ray &ray, HitInfo &hInfo, int hitSide=HIT_FRONT ) const;
+	virtual void ViewportDisplay() const;
+};
+
+extern Sphere theSphere;
+
+//-------------------------------------------------------------------------------
+
+#endif
