a4c7ef96034ebb6330cead7f0ea0e93c98fa1288
Hongbin Zhang <zhan2385@umn.edu>
2017-08-23 16:04:31 -0600

    Added Scene loading code

--- main.cpp
+++ main.cpp
@@ -2,16 +2,17 @@
 #include "ExternalLibrary/scene.h"
 #include "ExternalLibrary/xmlload.cpp"
 
-
-
 Camera camera = Camera();
 RenderImage renderImage = RenderImage();
 Sphere theSphere = Sphere();
 Node rootNode = Node();
 
+
+
 int main(int argc, const char* argv[]) 
 {
 	//Load Scene
+    LoadScene("/Users/Peter/GitRepos/RayTracer-Utah/SceneFiles/Project1.xml");
     
 	//Perform Rendering
 
