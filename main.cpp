25d7011de3c41c8a19f462e7867ba9cd28e3f0a1
Peter Zhang <Peter@Peters-MacBook.local>
2017-08-22 21:28:43 -0600

    Reduced initial compiling error to 2

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "ExternalLibrary/objects.h"
+#include "ExternalLibrary/scene.h"
+#include "ExternalLibrary/xmlload.cpp"
+
+Camera camera;
+RenderImage renderImage;
+Sphere theSphere;
+Node rootNode;
+
+
+int main(int argc, const char* argv[]) 
+{
+	//Load Scene
+    LoadScene("Test");
+    
+	//Perform Rendering
+
+	//Output Image
+}
